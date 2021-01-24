2f765848bac3d9eb4f58fe6db2ad24c71bfd1464
TE Schouten <thijs.schouten@gmail.com>
2021-01-24 20:30:50 +0100

    Renamed scriptfile

---
+++ Dashboard/assets/script.js
@@ -0,0 +1,14 @@
+console.log("Custom script loaded.")
+// if (!window.dash_clientside) {
+//     window.dash_clientside = {};
+// }
+// window.dash_clientside.clientside = {
+//     resize: function(value) {
+//         console.log("resizing..."); // for testing
+//         setTimeout(function() {
+//             window.dispatchEvent(new Event("resize"));
+//             console.log("fired resize");
+//         }, 500);
+//     return null;
+//     },
+// };
